946599904a8d182ea2513b30665e574f14d0e509
Cishy <fedorovnikita04@yandex.ru>
2024-01-23 13:38:49 +0300

    Репозитории in-memory и валидатор дубликатов почт помечены устаревшими

--- src/main/java/ru/practicum/shareit/item/repository/InMemoryItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/InMemoryItemRepository.java
@@ -1,19 +1,17 @@
 package ru.practicum.shareit.item.repository;
 
-import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.exception.EntityAlreadyExistsException;
-import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.entity.Item;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-@Repository
-public class InMemoryItemRepository implements ItemRepository {
+@Deprecated
+public class InMemoryItemRepository {
 
     private int idCounter = 0;
     private final Map<Integer, Item> items = new HashMap<>();
 
-    @Override
     public Item save(Item item) {
         if (item.getId() != null && findById(item.getId()).isPresent()) {
             throw new EntityAlreadyExistsException("Предмет с таким id уже существует");
@@ -24,17 +22,14 @@ public class InMemoryItemRepository implements ItemRepository {
         return item;
     }
 
-    @Override
     public Optional<Item> findById(int id) {
         return Optional.ofNullable(items.get(id));
     }
 
-    @Override
     public List<Item> findAll() {
         return new ArrayList<>(items.values());
     }
 
-    @Override
     public List<Item> findAllItemsOfUser(int ownerId) {
         return findAll().stream()
                 .filter(item -> item.getOwner().getId().equals(ownerId))
