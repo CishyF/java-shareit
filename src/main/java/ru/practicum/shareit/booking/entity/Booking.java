68ec5d8cf91023e84f454893c74da4ee576e982c
Cishy <fedorovnikita04@yandex.ru>
2024-01-23 13:25:54 +0300

    Сущностям добавлены аннотации для работы с jpa

---
+++ src/main/java/ru/practicum/shareit/booking/entity/Booking.java
@@ -0,0 +1,50 @@
+package ru.practicum.shareit.booking.entity;
+
+import lombok.*;
+import ru.practicum.shareit.item.entity.Item;
+import ru.practicum.shareit.user.entity.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "booking")
+@Getter @Setter @ToString
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "start_at")
+    private LocalDateTime start;
+
+    @Column(name = "end_at")
+    private LocalDateTime end;
+
+    @Enumerated(EnumType.ORDINAL)
+    private BookingStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private User booker;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Booking booking = (Booking) o;
+
+        return id.equals(booking.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
---
+++ src/main/java/ru/practicum/shareit/item/entity/Item.java
@@ -0,0 +1,46 @@
+package ru.practicum.shareit.item.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.practicum.shareit.user.entity.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+@Getter @Setter @ToString
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(name = "is_available")
+    private Boolean available;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private User owner;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        return id.equals(item.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
