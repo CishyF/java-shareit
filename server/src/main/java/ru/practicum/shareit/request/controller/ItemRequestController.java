daeb98427ef35a4de9300bbb48766f892bc85688
Cishy <fedorovnikita04@yandex.ru>
2024-03-17 14:47:40 +0300

    Возвращено логирование запросов в ItemRequestController

--- server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -21,9 +21,12 @@ public class ItemRequestController {
 
     @GetMapping
     public List<ItemRequestDtoResponse> getRequests(@RequestHeader("X-Sharer-User-Id") int requestorId) {
-        return requestMapper.itemRequestsToResponseDtos(
+        log.info("Пришел GET-запрос /requests, userId={} без тела", requestorId);
+        List<ItemRequestDtoResponse> dtos = requestMapper.itemRequestsToResponseDtos(
                 requestService.findRequestsOfUser(requestorId)
         );
+        log.info("Ответ на GET-запрос /requests, userId={} с телом={}", requestorId, dtos);
+        return dtos;
     }
 
     @GetMapping("/all")
@@ -32,21 +35,30 @@ public class ItemRequestController {
             @RequestParam int from,
             @RequestParam int size
     ) {
-        return requestMapper.itemRequestsToResponseDtos(
+        log.info("Пришел GET-запрос /requests/all?from={}&size={}, userId={} без тела", from, size, userId);
+        List<ItemRequestDtoResponse> dtos = requestMapper.itemRequestsToResponseDtos(
                 requestService.findRequestsOfOtherUsers(userId, from, size)
         );
+        log.info("Ответ на GET-запрос /requests/all?from={}&size={}, userId={} с телом={}", from, size, userId, dtos);
+        return dtos;
     }
 
     @GetMapping("/{requestId}")
     public ItemRequestDtoResponse getRequestById(@RequestHeader("X-Sharer-User-Id") int requestorId, @PathVariable int requestId) {
+        log.info("Пришел GET-запрос /requests/{requestId={}}, userId={} без тела", requestId, requestorId);
         requestService.validateRequestor(requestorId);
-        return requestMapper.itemRequestToResponseDto(requestService.findById(requestId));
+        ItemRequestDtoResponse dto = requestMapper.itemRequestToResponseDto(requestService.findById(requestId));
+        log.info("Ответ на GET-запрос /requests/{requestId={}}, userId={} с телом={}", requestId, requestorId, dto);
+        return dto;
     }
 
     @PostMapping
     public ItemRequestDtoResponse createRequest(@RequestHeader("X-Sharer-User-Id") int requestorId, @RequestBody ItemRequestDtoRequest dto) {
-        return requestMapper.itemRequestToResponseDto(
+        log.info("Пришел POST-запрос /requests, userId={} с телом={}", requestorId, dto);
+        ItemRequestDtoResponse savedDto = requestMapper.itemRequestToResponseDto(
                 requestService.create(requestorId, dto)
         );
+        log.info("Ответ на POST-запрос /requests, userId={} с телом={}", requestorId, savedDto);
+        return savedDto;
     }
 }
